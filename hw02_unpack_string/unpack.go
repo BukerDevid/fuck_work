d680755fd866aa8707ff89ec6d9c2aaa67f0db45
@v.guselnikov <v.guselnikov@greatbook.ru>
2022-12-15 19:08:24 +0300

    Add new function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,74 +10,85 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(v string) (string, error) {
+	if v == "" {
+		return "", nil
+	}
+
 	var baffle, number bool
 	result := &strings.Builder{}
 	smbRep := &strings.Builder{}
 
-	if unicode.IsDigit(rune(v[0])) || v == "" {
+	if unicode.IsDigit(rune(v[0])) {
 		return "", ErrInvalidString
 	}
 
 	for _, currRune := range v {
-		if currRune == '\\' {
-			number = false
-			if baffle {
-				if smbRep.Len() == 0 {
-					smbRep.WriteRune('\\')
-					baffle = false
-					continue
-				}
-				result.WriteString(smbRep.String())
-				smbRep.Reset()
-				continue
-			}
-			baffle = true
-			continue
+		if err := check(&baffle, &number, &currRune, result, smbRep); err != nil {
+			return "", err
 		}
-
-		if num, err := strconv.Atoi(string(currRune)); err == nil {
-			if number {
-				return result.String(), ErrInvalidString
-			}
-
-			if baffle {
-				smbRep.WriteRune(currRune)
-				baffle = false
-				continue
-			}
-
-			number = true
-			if smbRep.Len() == 0 {
-				buf := result.String()
-				result.Reset()
-				result.WriteString(buf[:len(buf)-1])
-				smbRep.WriteRune(rune(buf[len(buf)-1]))
-			}
-
-			baffle = false
-			if num != 0 {
-				result.WriteString(strings.Repeat(smbRep.String(), num))
-				smbRep.Reset()
-			}
-			continue
-		}
-
-		if baffle {
-			if smbRep.Len() == 0 {
-				smbRep.WriteRune('\\')
-				baffle = false
-			}
-
-			smbRep.WriteRune(currRune)
-			continue
-		}
-
-		result.WriteRune(currRune)
-		number = false
 	}
 	if baffle {
 		return result.String(), ErrInvalidString
 	}
 	result.WriteString(smbRep.String())
 	return result.String(), nil
+}
+
+func check(baffle, number *bool, currRune *rune, result, smbRep *strings.Builder) error {
+	if *currRune == '\\' {
+		*number = false
+		if *baffle {
+			if smbRep.Len() == 0 {
+				smbRep.WriteRune('\\')
+				*baffle = false
+				return nil
+			}
+			result.WriteString(smbRep.String())
+			smbRep.Reset()
+			return nil
+		}
+		*baffle = true
+		return nil
+	}
+
+	if num, err := strconv.Atoi(string(*currRune)); err == nil {
+		if *number {
+			return ErrInvalidString
+		}
+
+		if *baffle {
+			smbRep.WriteRune(*currRune)
+			*baffle = false
+			return nil
+		}
+
+		*number = true
+		if smbRep.Len() == 0 {
+			buf := result.String()
+			result.Reset()
+			result.WriteString(buf[:len(buf)-1])
+			smbRep.WriteRune(rune(buf[len(buf)-1]))
+		}
+
+		*baffle = false
+		if num != 0 {
+			result.WriteString(strings.Repeat(smbRep.String(), num))
+		}
+		smbRep.Reset()
+		return nil
+	}
+
+	if *baffle {
+		if smbRep.Len() == 0 {
+			smbRep.WriteRune('\\')
+			*baffle = false
+		}
+
+		smbRep.WriteRune(*currRune)
+		return nil
+	}
+
+	result.WriteRune(*currRune)
+	*number = false
+	return nil
 }

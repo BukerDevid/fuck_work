35bc292a63185254d9eca7ff05ffa0db0b492faa
BookerDevid <mr.wgw@yandex.ru>
2023-01-22 22:15:28 +0300

    Add base logic (without tests)

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,7 +13,7 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
+	mx sync.RWMutex
 
 	capacity int
 	queue    List
@@ -21,10 +25,67 @@ type cacheItem struct {
 	value interface{}
 }
 
+func (c *cacheItem) GetValue() interface{} { return c.value }
+
+func (c *cacheItem) GetKey() Key { return c.key }
+
 func NewCache(capacity int) Cache {
 	return &lruCache{
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
 	}
+}
+
+func (lc *lruCache) Set(key Key, value interface{}) bool {
+	lc.mx.Lock()
+	defer lc.mx.Unlock()
+
+	if item, ok := lc.items[key]; ok {
+
+		item.Value = cacheItem{
+			key:   key,
+			value: value,
+		}
+
+		lc.queue.MoveToFront(item)
+		return true
+	}
+
+	if lc.queue.Len() == lc.capacity {
+		rmItem := lc.queue.Back()
+
+		//INFO: небезопасная дрянь
+		key := rmItem.Value.(cacheItem)
+
+		delete(lc.items, key.GetKey())
+		lc.queue.Remove(rmItem)
+	}
+
+	lc.items[key] = lc.queue.PushFront(cacheItem{
+		key:   key,
+		value: value,
+	})
+
+	return false
+}
+
+func (lc *lruCache) Get(key Key) (interface{}, bool) {
+	lc.mx.Lock()
+	defer lc.mx.Unlock()
+
+	if item, ok := lc.items[key]; ok {
+		cacheValue := item.Value.(cacheItem)
+		return cacheValue.GetValue(), ok
+	}
+
+	return nil, false
+}
+
+func (lc *lruCache) Clear() {
+	lc.mx.Lock()
+	defer lc.mx.Unlock()
+
+	lc.items = make(map[Key]*ListItem, lc.capacity)
+	lc.queue = NewList()
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,8 +17,128 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	size int32
+
+	first *ListItem
+	last  *ListItem
+}
+
+func (ls *list) Len() int {
+	return (int)(ls.size)
+}
+
+func (ls *list) Front() *ListItem { return ls.first }
+
+func (ls *list) Back() *ListItem { return ls.last }
+
+func (ls *list) checkFirst(new *ListItem) bool {
+	if ls.first == nil {
+		ls.first = new
+		ls.last = new
+		return false
+	}
+	return true
+}
+
+func (ls *list) checkRelation() {
+	if ls.size == 2 {
+		if ls.first == nil && ls.last != nil {
+			ls.first = ls.last
+		} else if ls.last == nil && ls.first != nil {
+			ls.last = ls.first
+		}
+	}
+}
+
+func (ls *list) PushFront(v interface{}) *ListItem {
+	defer func() {
+		ls.size++
+	}()
+
+	newItem := &ListItem{
+		Value: v,
+	}
+
+	if !ls.checkFirst(newItem) {
+		return ls.first
+	}
+
+	buf := ls.first
+	buf.Prev = newItem
+
+	ls.first = newItem
+	ls.first.Next = buf
+
+	return ls.first
+}
+
+func (ls *list) PushBack(v interface{}) *ListItem {
+	defer func() {
+		ls.size++
+	}()
+
+	newItem := &ListItem{
+		Value: v,
+	}
+
+	if !ls.checkFirst(newItem) {
+		return ls.first
+	}
+
+	buf := ls.last
+	buf.Next = newItem
+
+	ls.last = newItem
+	ls.last.Prev = buf
+
+	return ls.last
+}
+
+func (ls *list) Remove(i *ListItem) {
+	defer func(i *ListItem) {
+		ls.size--
+		ls.checkRelation()
+	}(i)
+
+	if ls.size == 0 {
+		return
+	}
+
+	if i.Next != nil && i.Prev != nil {
+		i.Next.Prev = i.Prev
+
+		if ls.last == i {
+			ls.last = i.Next
+		}
+
+		i.Prev.Next = i.Next
+
+		if ls.first == i {
+			ls.first = i.Prev
+		}
+	}
+}
+
+func (ls *list) MoveToFront(i *ListItem) {
+	if ls.size < 2 || ls.first == i {
+		return
+	}
+
+	buf := ls.first
+	buf.Prev = i
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	}
+
+	ls.first = i
+	ls.first.Next = buf
+	ls.first.Prev = nil
+
 }
 
 func NewList() List {

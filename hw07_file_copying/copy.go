80458add669c21b1b9efc0af0e17902d1e98e71d
@v.guselnikov <v.guselnikov@greatbook.ru>
2023-02-01 18:16:39 +0300

    Without tests

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
-	"io/fs"
 	"log"
 	"os"
 	"time"
@@ -12,43 +12,114 @@ import (
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrLimitExceedsFileSize  = errors.New("limits smaller file size sub offset")
 	ErrInvalidPath           = errors.New("invalid path")
+	ErrFileIsNilPointer      = errors.New("use nil pointer as file")
 	ErrTmpCreate             = errors.New("cannot create tmp file")
 )
 
-type OffsetReader struct {
-	src io.Reader
+const BUF_SIZE = 64 //kB
+
+type FileCop struct {
+	file *os.File
+	out  *os.File
+	info os.FileInfo
 }
 
-func prepFile(fromPath string) (src *os.File, out *os.File, err error) {
-	src, err = os.OpenFile(fromPath, os.O_RDONLY, os.ModePerm)
+func NewFileCop(fromPath string) (*FileCop, error) {
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, os.ModePerm)
 	if err != nil {
 		log.Print(time.Now(), err)
-		err = ErrInvalidPath
-		return
+		return nil, ErrInvalidPath
 	}
 
-	out, err = os.CreateTemp("tmp_copy", "*")
+	info, err := src.Stat()
 	if err != nil {
 		log.Print(time.Now(), err)
-		err = ErrInvalidPath
-		return
+		return nil, ErrUnsupportedFile
 	}
 
-	return
+	if info.IsDir() {
+		return nil, ErrUnsupportedFile
+	}
+
+	out, err := os.CreateTemp("tmp_copy", "*")
+	if err != nil {
+		log.Print(time.Now(), err)
+		return nil, ErrInvalidPath
+	}
+
+	return &FileCop{
+		file: src,
+		out:  out,
+		info: info,
+	}, nil
 }
 
-func checkSize(stat func() (fs.FileInfo, error), offset int64) error {
-	fileInfo, err := stat()
-	if err != nil {
-		return ErrUnsupportedFile
-	}
-
-	if fileInfo.Size() < offset {
+func (cf *FileCop) checkOffsetAndLimit(offset, limit int64) error {
+	if cf.info.Size() <= offset || cf.info.Size() <= 0 {
 		return ErrOffsetExceedsFileSize
 	}
 
+	if cf.info.Size()-offset < limit {
+		return ErrOffsetExceedsFileSize
+	}
 	return nil
+}
+
+func (cf *FileCop) Copy(offset, limit int64, progress chan uint8) {
+	if limit < 0 {
+		return
+	}
+	step := (cf.info.Size() / (BUF_SIZE * 1024)) + 1
+	buf := make([]byte, BUF_SIZE*1024)
+	// countScan := 0
+
+	// defer func() {
+	// 	if countScan != int(cf.info.Size()-offset) {
+	// 		log.Print("invalid copy. unknow error")
+	// 	}
+	// }()
+
+	for currentStep := int64(0); currentStep < step; currentStep++ {
+		n, err := cf.file.ReadAt(buf, offset)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+		}
+
+		if n > 0 {
+			x, err := cf.out.Write(buf)
+			if err != nil {
+				panic(err)
+			}
+
+			if n != x {
+				log.Printf("invalid step - scan %d, write %d", n, x)
+			}
+			progress <- uint8((currentStep / step) * 100)
+		}
+	}
+
+	close(progress)
+}
+
+func (cf *FileCop) ReadInBuf(offset, limit int64) (chan uint8, error) {
+	progress := make(chan uint8, 100)
+
+	if err := cf.checkOffsetAndLimit(offset, limit); err != nil {
+		return nil, err
+	}
+
+	go cf.Copy(offset, limit, progress)
+
+	return progress, nil
+}
+
+func (cf *FileCop) Close() {
+	cf.file.Close()
+	cf.out.Close()
 }
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
@@ -56,24 +127,18 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 		return ErrInvalidPath
 	}
 
-	src, out, err := prepFile(fromPath)
+	cf, err := NewFileCop(fromPath)
 	if err != nil {
 		return err
 	}
 
-	defer func() {
-		os.Remove(out.Name())
-	}()
+	progress, err := cf.ReadInBuf(offset, limit)
 
-	if err := checkSize(src.Stat, offset); err != nil {
-		return err
+	for step := range progress {
+		fmt.Printf("\rprogress - %d %%", step)
 	}
 
-	if limit > 0 {
-		_, err = io.CopyN(out, src, limit)
-	} else {
-		_, err = io.Copy(out, src)
-	}
+	cf.Close()
 
 	if err != nil {
 		return err
